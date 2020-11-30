fa7f4142c03b6768f7fd504e504cef570522ca6e
Lawrence Jasper <larry.jasper2@gmail.com>
2020-11-30 18:46:42 -0500

    clean up 2

--- Root/script.js
+++ Root/script.js
@@ -1,14 +1,15 @@
 let slideIndex = 0;
 function slide() {
-    let mySlides = document.getElementsByClassName("mySlides");
-    for (let i = 0; i < mySlides.length; i++) {
+    let mySlides = document.getElementsByClassName("mySlides");//grabs the element
+    for (let i = 0; i < mySlides.length; i++) {//for loop each slide amd alternate the display setting
         mySlides[i].style.display = "none";
     }
-    slideIndex++;
+    slideIndex++;//counter
     if (slideIndex > mySlides.length) {slideIndex = 1}
     mySlides[slideIndex-1].style.display = "block";
     setTimeout(slide, 2000); // Change image every 2 seconds
 }
-slide();
+slide();//runs the function
 
 
+//by Lawrence Jasper
